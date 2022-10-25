a0fd51b45e964453f307ac1eddcac07da9e38776
Danny <danny.n.kha@gmail.com>
2022-10-25 11:53:23 -0700

    fixing about and contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
@@ -8,7 +8,18 @@ import Contact from './components/Contact'
 function App() {
   return (
     <>
-      <Routes>
+        <ul>
+          <li>
+            <Link  to="/">Home</Link>
+          </li>
+          <li>
+            <Link  to="/about">About</Link>
+          </li>
+          <li>
+            <Link  to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
@@ -16,7 +27,7 @@ function App() {
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
 export default App
